01aeb7393d045bb7383d2b6e1a72b19bba7ed9ee
Joe Irby <jbirby2@gmail.com>
2019-01-05 15:07:33 -0600

    tiny commit to add client token caching before I forget

--- TunnelQuest.TunnelWatcher/TokenGenerator.cs
+++ TunnelQuest.TunnelWatcher/TokenGenerator.cs
@@ -9,30 +9,33 @@ namespace TunnelQuest.TunnelWatcher
 {
     internal static class TokenGenerator
     {
+        private static string cachedToken = null;
 
         public static string GetAuthToken()
         {
-            string tokenValue = "";
+            if (cachedToken != null)
+                return cachedToken;
+
             ManagementClass mc = new ManagementClass("win32_processor");
             ManagementObjectCollection moc = mc.GetInstances();
 
             foreach (ManagementObject mo in moc)
             {
-                if (tokenValue == "")
+                if (cachedToken == null)
                 {
                     //Get only the first CPU's ID
-                    tokenValue = mo.Properties["processorID"].Value.ToString();
+                    cachedToken = mo.Properties["processorID"].Value.ToString();
                     break;
                 }
             }
 
-            if (String.IsNullOrWhiteSpace(tokenValue))
+            if (String.IsNullOrWhiteSpace(cachedToken))
                 throw new Exception("Could not find computer's processor ID");
 
             // STUB TODO - do some sort of one-way hash so that we're not sending the server
             // our actual processor ID, but instead some random string seeded by the processor ID.
 
-            return tokenValue;
+            return cachedToken;
         }
     }
 }
